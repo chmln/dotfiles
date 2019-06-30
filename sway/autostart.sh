773d060a87720f93b379ad398fe96ebfeab25159
Greg <gregory.mkv@gmail.com>
2019-06-30 16:16:20 -0400

    sway - use systemd to autostart services

--- sway/autostart.sh
+++ sway/autostart.sh
@@ -1,18 +1,2 @@
 #!/bin/sh
 
-pkill devmon
-pkill swaybg
-pkill redshift
-
-sh ~/.config/sway/waybar.sh &
-
-swaybg -o "*" -i ~/.config/wallpaper &
-
-# blue-light filter
-redshift -c ~/.config/redshift.conf -m wayland &
-
-# auto-mount plugged in usb devices
-devmon &
-
-# check for missed email notifications
-~/.config/mbsync/boot.sh &
