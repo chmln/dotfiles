83f16f2387402d0bdbff1b287884a96c7da07464
Greg <gregory.mkv@gmail.com>
2019-09-02 22:24:59 -0400

    Remove sway and alacritty
    Hello nvidia

--- sway/monitors.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/fish
-set -l num_monitors (swaymsg -t get_outputs | jq -r '. | length')
-
-if [ $num_monitors -gt 1 ]
-  swaymsg "output eDP-1 pos 320 1440 mode 1920x1080"
-  swaymsg "output HDMI-A-1 pos 0 0 mode 2560x1440"
-end
-
