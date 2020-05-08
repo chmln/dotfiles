68d0c843bf97a353ffe964d0dd56760602dafa58
Gregory <gregory.mkv@gmail.com>
2020-05-08 00:11:59 -0400

    update shell colors

--- fish/variables.fish
+++ fish/variables.fish
@@ -42,7 +42,7 @@ set -U fish_color_selection 'white' '--bold' '--background=brblack'
 set -U fish_color_status red
 set -U fish_color_user brgreen
 set -U fish_color_valid_path --underline
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description yellow
+set -U fish_pager_color_completion normal --bold
+set -U fish_pager_color_description brwhite
 set -U fish_pager_color_prefix 'white' '--bold' '--underline'
 set -U fish_pager_color_progress 'brwhite' '--background=cyan'
